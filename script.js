d415dd56ee6e02d4e284f4333cbc4ddbfd232dbe
raiduff <lovelyofqueen@gmail.com>
2024-08-15 20:45:52 +0530

    Add files via upload
    added index.html

---
+++ script.js
@@ -0,0 +1,95 @@
+// Selecting form and input elements
+const form = document.querySelector("form");
+const passwordInput = document.getElementById("password");
+const passToggleBtn = document.getElementById("pass-toggle-btn");
+
+// Function to display error messages
+const showError = (field, errorText) => {
+    field.classList.add("error");
+    const errorElement = document.createElement("small");
+    errorElement.classList.add("error-text");
+    errorElement.innerText = errorText;
+    field.closest(".form-group").appendChild(errorElement);
+}
+
+// Function to handle form submission
+const handleFormData = (e) => {
+    e.preventDefault();
+
+    // Retrieving input elements
+    const fullnameInput = document.getElementById("fullname");
+    const emailInput = document.getElementById("email");
+    const dateInput = document.getElementById("date");
+    const genderInput = document.getElementById("gender");
+    const loginidInput = document.getElementById("loginid");
+
+
+    // Getting trimmed values from input fields
+    const fullname = fullnameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+    const date = dateInput.value;
+    const gender = genderInput.value;
+    const loginid = loginidInput.value.trim();
+
+    // Regular expression pattern for email validation
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    // Clearing previous error messages
+    document.querySelectorAll(".form-group .error").forEach(field => field.classList.remove("error"));
+    document.querySelectorAll(".error-text").forEach(errorText => errorText.remove());
+
+    // Performing validation checks
+    if (fullname === "") {
+        showError(fullnameInput, "Name is required");
+    }
+    if (gender === "") {
+        showError(genderInput, "Select your gender");
+    }
+    if (!emailPattern.test(email)) {
+        showError(emailInput, "Enter a valid email address");
+    }
+    if (password === "") {
+        showError(passwordInput, "Password is required");
+    }
+    if (loginid === "") {
+        showError(loginidInput, "Enter your mobile number or email");
+    }
+
+    // Checking for any remaining errors before form submission
+    const errorInputs = document.querySelectorAll(".form-group .error");
+    if (errorInputs.length > 0) return;
+
+    // Submitting the form
+    form.submit();
+}
+
+// Toggling password visibility
+passToggleBtn.addEventListener('click', () => {
+    passToggleBtn.className = passwordInput.type === "password" ? "fa-solid fa-eye-slash" : "fa-solid fa-eye";
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+});
+
+// Handling form submission event
+form.addEventListener("submit", handleFormData);
+const selectBtn = document.querySelector(".select-btn"),
+      items = document.querySelectorAll(".item");
+
+selectBtn.addEventListener("click", () => {
+    selectBtn.classList.toggle("open");
+});
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("checked");
+
+        let checked = document.querySelectorAll(".checked"),
+            btnText = document.querySelector(".btn-text");
+
+            if(checked && checked.length > 0){
+                btnText.innerText = `${checked.length} Selected`;
+            }else{
+                btnText.innerText = "Confirmation";
+            }
+    });
+})
